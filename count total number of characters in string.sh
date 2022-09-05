8f4b09b0e1b0b54c71b43cfc8eec27645494a23c
IamGodddddd <112825765+IamGodddddd@users.noreply.github.com>
2022-09-05 15:20:08 +0530

    Delete count total number of characters in string.sh

--- count total number of characters in string.sh
+++
@@ -1,8 +0,0 @@
-clear
-echo "Enter the string"
-read char
-echo "To count number of character in string"
-echo "======================================"
-len=`echo $char | wc -c`
-hlen=` expr $len - 1 `
-echo "number of character is = $hlen"
